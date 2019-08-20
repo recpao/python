0106a288a41038074496f218e38f210df6103ff1
paolo recanatini <precanatini@gmail.com>
2019-08-20 18:03:03 +0200

    Add files via upload

---
+++ lpthw/ex3.py
@@ -0,0 +1,13 @@
+print("** Ora conterò il pollame **")
+print("Galline: ", 25 + 30 /6)
+print("Galli: ", 100 - 25 * 3 % 4)
+print("** Ora conterò le uova **")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print("3 + 2 < 5 - 7, vero?")
+print(3 + 2 < 5 - 7)
+
+print("Quanto fa 3 + 2?", 3 + 2)
+print("Quanto fa 5 -7?", 5 - 7)
+print("Ecco perchè è falso")
+
+ 
